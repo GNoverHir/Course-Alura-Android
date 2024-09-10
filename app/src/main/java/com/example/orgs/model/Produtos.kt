0c3c450a2b2196400bb04e9737a557b4c1069dc0
Pedro Lucca <pedroluccaprofissional@gmail.com>
2024-09-09 21:14:20 -0300

    feat: adding new button features

--- app/src/main/java/com/example/orgs/model/Produtos.kt
+++ app/src/main/java/com/example/orgs/model/Produtos.kt
@@ -2,7 +2,8 @@ package com.example.orgs.model
 
 import java.math.BigDecimal
 
-class Produtos (
+// É O MESMO DATA DO SPRING (We are the champions tocando no fundo*)
+data class Produtos (
     val nome: String,
     val descricao: String,
     val valor: BigDecimal
--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,11 +1,17 @@
 package com.example.orgs.ui.activity
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.orgs.R
+import com.example.orgs.model.Produtos
+import java.math.BigDecimal
+import kotlin.math.log
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,35 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button = findViewById<Button>(R.id.saveButton)
+        button.setOnClickListener{
+            val nameEditText = findViewById<EditText>(R.id.nomeEditText)
+            val name = nameEditText.text.toString()
+
+            val descEditText = findViewById<EditText>(R.id.descricaoEditText)
+            val desc = descEditText.text.toString()
+
+            val valueEditText = findViewById<EditText>(R.id.valorEditText)
+            val stringValue = valueEditText.text.toString()
+            // Caso passe um valor em branco, bigDecimal vai quebrar. Então aqui fazemos uma pequena verificação.
+            //Caso tenha valor em branco, definir como 0. Caso contrario, converta a string em bigDecimal
+            val value = if (stringValue.isBlank()){
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(stringValue)
+            }
+
+
+            val produtoNovo = Produtos(
+                nome = name,
+                descricao = desc,
+                valor = value
+            )
+
+
+            Log.i("FormularioProduto", "onCreate $produtoNovo")
         }
     }
 }
